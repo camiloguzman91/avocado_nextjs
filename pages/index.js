5a5ac128a60b21875acc711a5ae2d000840800fe
Camilo Guzman <camiloge_91@hotmail.com>
2021-02-19 18:39:50 -0500

    linking

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <h1>Hi Platzi</h1>
     </div>
   );
